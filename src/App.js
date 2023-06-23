4f70b0bbe8ed1cc97f41e6fa1b35f6844c1b46b0
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2023-06-23 23:38:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function Avatar({person, size}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <img
+      className="avatar"
+      src="https://i.imgur.com/1bX5QH6.jpg"
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+    />
   );
 }
 
-export default App;
+export default function Profile() {
+  return <Avatar person={{ name: 'Lin Lanying', imageId: '1bX5QH6' }} size={100}/>;
+}
