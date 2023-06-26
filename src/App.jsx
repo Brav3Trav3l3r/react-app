b2d25b5de6b3e0247f4b3f5817483ae906dba10c
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2023-06-26 22:10:22 +0530

    artwork

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,80 @@
-import { useState } from "react";
+import { useImmer } from 'use-immer';
 
-export default function App() {
-  const [value, setValue] = useState("hELLO");
+export default function Form() {
+  const [person, updatePerson] = useImmer({
+    name: 'Niki de Saint Phalle',
+    artwork: {
+      title: 'Blue Nana',
+      city: 'Hamburg',
+      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    }
+  });
 
-  function handleChange(e) {
-    setValue(e.target.value);
+  function handleNameChange(e) {
+    updatePerson(draft => {
+      draft.name = e.target.value;
+    });
+  }
+
+  function handleTitleChange(e) {
+    updatePerson(draft => {
+      draft.artwork.title = e.target.value;
+    });
+  }
+
+  function handleCityChange(e) {
+    updatePerson(draft => {
+      draft.artwork.city = e.target.value;
+    });
+  }
+
+  function handleImageChange(e) {
+    updatePerson(draft => {
+      draft.artwork.image = e.target.value;
+    });
   }
 
   return (
-    <div className=" p-4">
-      <input
-        className="border-2"
-        type="text"
-        value={value}
-        onChange={handleChange}
+    <>
+      <label>
+        Name:
+        <input
+          value={person.name}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label>
+        Title:
+        <input
+          value={person.artwork.title}
+          onChange={handleTitleChange}
+        />
+      </label>
+      <label>
+        City:
+        <input
+          value={person.artwork.city}
+          onChange={handleCityChange}
+        />
+      </label>
+      <label>
+        Image:
+        <input
+          value={person.artwork.image}
+          onChange={handleImageChange}
+        />
+      </label>
+      <p>
+        <i>{person.artwork.title}</i>
+        {' by '}
+        {person.name}
+        <br />
+        (located in {person.artwork.city})
+      </p>
+      <img 
+        src={person.artwork.image} 
+        alt={person.artwork.title}
       />
-
-      <h1>{value}</h1>
-    </div>
+    </>
   );
 }
